4b6c51a6750bf7f90bb396da83d9d431f3c22f45
sarazebardast <sara.zebardast03@gmail.com>
2024-08-01 11:27:10 +0300

    security

--- src/api.js
+++ src/api.js
@@ -4,4 +4,21 @@ const instance = axios.create({
     baseURL: 'http://localhost:8080', // Adjust the baseURL according to your backend setup
 });
 
+// Add a request interceptor
+instance.interceptors.request.use(
+    (config) => {
+        // You can use either the 'user' or 'sara' credentials here
+        const username = 'sara'; // or 'user'
+        const password = 'sara'; // or 'password'
+
+        const base64Credentials = btoa(`${username}:${password}`);
+        config.headers['Authorization'] = `Basic ${base64Credentials}`;
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 export default instance;
